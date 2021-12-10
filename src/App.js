d6c606a11a935c7d74184814a91f7d071578c6af
Kadir Zukorlic <kadirzukorlic@gmail.com>
2021-12-10 03:37:57 +0100

    all mock tasks listed

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
-import './App.css';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import ToDo from './pages/ToDo';
+import { ToDo } from './pages/ToDo';
+import { Box } from '@material-ui/core';
+import './styles/styles.scss';
 
 function App() {
     return (
-        <BrowserRouter>
-            <Switch>
-                <Route path={'/'} exact component={ToDo} />
-            </Switch>
-        </BrowserRouter>
+        <Box className="app">
+            <BrowserRouter>
+                <Switch>
+                    <Route path={'/'} exact component={ToDo} />
+                </Switch>
+            </BrowserRouter>
+        </Box>
     );
 }
 
--- src/pages/ToDo.js
+++ src/pages/ToDo.js
@@ -1,7 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Grid, Box, Button } from '@material-ui/core';
 
-const ToDo = () => {
-    return <div>TO-DO list</div>;
+const mockTasks = [
+    {
+        name: 'Task 1',
+        description: 'Buy something',
+        date: new Date(),
+        priority: 'Low',
+        completed: false,
+    },
+    {
+        name: 'Task 2',
+        description: 'Sell something',
+        date: new Date(),
+        priority: 'High',
+        completed: true,
+    },
+];
+
+export const ToDo = () => {
+    const [tasks, setTasks] = useState(mockTasks);
+
+    return (
+        <Grid container className="todo-container">
+            {tasks.map((task, i) => (
+                <Grid item key={i} xs={12} sm={10} md={8}>
+                    <Box className="todo-item">
+                        <Box>{task.name}</Box>
+                        <Box display="flex">
+                            <Button>Edit</Button>
+                            <Button>Delete</Button>
+                            {!task.completed && <Button>Complete</Button>}
+                        </Box>
+                    </Box>
+                </Grid>
+            ))}
+        </Grid>
+    );
 };
-
-export default ToDo;
