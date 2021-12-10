8fae1a1dfbd796be58173efc8e4e2941d111eb3e
Kadir Zukorlic <kadirzukorlic@gmail.com>
2021-12-10 03:43:53 +0100

    set task to be completed

--- src/pages/ToDo.js
+++ src/pages/ToDo.js
@@ -3,6 +3,7 @@ import { Grid, Box, Button } from '@material-ui/core';
 
 const mockTasks = [
     {
+        id: 1,
         name: 'Task 1',
         description: 'Buy something',
         date: new Date(),
@@ -10,6 +11,7 @@ const mockTasks = [
         completed: false,
     },
     {
+        id: 2,
         name: 'Task 2',
         description: 'Sell something',
         date: new Date(),
@@ -17,20 +19,38 @@ const mockTasks = [
         completed: true,
     },
 ];
-
 export const ToDo = () => {
     const [tasks, setTasks] = useState(mockTasks);
+
+    const setCompleted = (id) => {
+        const newTasks = tasks.map((task) => {
+            if (task.id === id) {
+                return { ...task, completed: true };
+            } else {
+                return task;
+            }
+        });
+        setTasks(newTasks);
+    };
 
     return (
         <Grid container className="todo-container">
             {tasks.map((task, i) => (
                 <Grid item key={i} xs={12} sm={10} md={8}>
-                    <Box className="todo-item">
+                    <Box
+                        className={`todo-item ${
+                            task.completed ? 'todo-item--completed' : ''
+                        }`}
+                    >
                         <Box>{task.name}</Box>
                         <Box display="flex">
                             <Button>Edit</Button>
                             <Button>Delete</Button>
-                            {!task.completed && <Button>Complete</Button>}
+                            {!task.completed && (
+                                <Button onClick={() => setCompleted(task.id)}>
+                                    Complete
+                                </Button>
+                            )}
                         </Box>
                     </Box>
                 </Grid>
